a2524f9585d04acce8c681f12880e3619b7382db
Thunder Liao <JERRY123789ZXP@hotmail.com>
2020-01-24 15:41:41 +0800

    First Edition

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace UUIDConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_player = new System.Windows.Forms.TextBox();
+            this.textBox_uuid = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel_Exception = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_url = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Directory Path:";
+            // 
+            // textBox_player
+            // 
+            this.textBox_player.BackColor = System.Drawing.Color.White;
+            this.textBox_player.Location = new System.Drawing.Point(8, 42);
+            this.textBox_player.Multiline = true;
+            this.textBox_player.Name = "textBox_player";
+            this.textBox_player.ReadOnly = true;
+            this.textBox_player.Size = new System.Drawing.Size(208, 285);
+            this.textBox_player.TabIndex = 4;
+            // 
+            // textBox_uuid
+            // 
+            this.textBox_uuid.BackColor = System.Drawing.Color.White;
+            this.textBox_uuid.Location = new System.Drawing.Point(235, 42);
+            this.textBox_uuid.Multiline = true;
+            this.textBox_uuid.Name = "textBox_uuid";
+            this.textBox_uuid.ReadOnly = true;
+            this.textBox_uuid.Size = new System.Drawing.Size(326, 285);
+            this.textBox_uuid.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Player Name:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox_player);
+            this.groupBox1.Controls.Add(this.textBox_uuid);
+            this.groupBox1.Location = new System.Drawing.Point(12, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(572, 332);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "List";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(233, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Online UUID:";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel_Exception,
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 394);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(592, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel_Exception
+            // 
+            this.toolStripStatusLabel_Exception.Name = "toolStripStatusLabel_Exception";
+            this.toolStripStatusLabel_Exception.Size = new System.Drawing.Size(63, 17);
+            this.toolStripStatusLabel_Exception.Text = "Error: N/A";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(200, 16);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(58, 17);
+            this.toolStripStatusLabel1.Text = "                 ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(138, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "UUID Source:";
+            // 
+            // textBox_url
+            // 
+            this.textBox_url.Location = new System.Drawing.Point(215, 34);
+            this.textBox_url.Name = "textBox_url";
+            this.textBox_url.Size = new System.Drawing.Size(358, 22);
+            this.textBox_url.TabIndex = 10;
+            this.textBox_url.Text = "https://api.mojang.com/users/profiles/minecraft/";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 416);
+            this.Controls.Add(this.textBox_url);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Player ID => Online UUID";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_player;
+        private System.Windows.Forms.TextBox textBox_uuid;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_Exception;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_url;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UUIDConverter
+{
+    public partial class Form1 : Form
+    {
+        List<string> playerID = new List<string>();
+        Thread Convert_Thread;
+        FolderBrowserDialog path;
+        DirectoryInfo d;
+        const string NO_DATA = "NO_DATA";
+        const string ERR_PAGE = "ERR_PAGE";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string getWebString(string id)
+        {
+            WebRequest myRequest = WebRequest.Create(textBox_url.Text + id);
+            myRequest.Method = "GET";
+            WebResponse myResponse = myRequest.GetResponse();
+            StreamReader sr = new StreamReader(myResponse.GetResponseStream());
+            string result = sr.ReadToEnd();
+            sr.Close();
+            myResponse.Close();
+            if (!result.Contains("id") && !result.Contains("name"))
+                return ERR_PAGE;
+            if (result.Contains("id"))
+                return result;
+            else
+                return NO_DATA;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox_player.Text = "";
+            this.textBox_uuid.Text = "";
+            path = new FolderBrowserDialog();
+            path.ShowDialog();
+            this.label1.Text = "Directory Path:" + path.SelectedPath;
+            d = new DirectoryInfo(path.SelectedPath);//Assuming Test is your Folder
+            Directory.CreateDirectory(d.FullName + "\\uuid");
+            update_btn(false);
+
+            Convert_Thread = new Thread(ConvertUUID);
+            Convert_Thread.IsBackground = true;
+            Convert_Thread.Start();
+        }
+
+        private void ConvertUUID()
+        {
+            //try
+            //{
+            FileInfo[] Files = d.GetFiles("*.dat"); //Getting Text files
+            if (Files.Length == 0)
+            {
+                update_btn(true);
+                update_err("No File");
+                Convert_Thread.Abort();
+            }
+            string str = "";
+            int i = 0;
+            foreach (FileInfo file in Files)
+            {
+                string id = file.Name.Split(new string[] { ".dat" }, StringSplitOptions.None)[0];
+                string raw = "", uuid = "";
+                //playerID.Add(file.Name.Split(new string[] { ".dat" }, StringSplitOptions.None)[0]);
+                if (id.Length <= 32)
+                {
+                    raw = getWebString(id);
+                    update_player_Log(id);
+                }
+                if (raw == ERR_PAGE)
+                {
+                    update_err("Error: Page error");
+                    Convert_Thread.Abort();
+                }
+                else if (raw == NO_DATA)
+                {
+                    update_uuid_Log("NO_DATA");
+                }
+                else if (raw != "")
+                {
+                    raw = raw.Split('"')[3];
+                    uuid = new Guid(raw).ToString();
+                    update_uuid_Log(uuid);
+                    File.Copy(file.FullName, Path.Combine(file.DirectoryName + "\\uuid", uuid + ".dat"), true);
+                }
+                update_progress(i, Files.Length);
+                Thread.Sleep(1000);
+                i++;
+
+                //if (i == 10)
+                //    break;
+            }
+            update_btn(true);
+            Process.Start("explorer.exe", d.FullName + "\\uuid");
+            //}
+            //catch (Exception e)
+            //{
+            //    update_err("Exception: " + e.ToString());
+            //}
+
+        }
+        private void update_progress(int progress, int total)
+        {
+            if (InvokeRequired)
+            {
+                this.BeginInvoke(new Action<int, int>(this.update_progress), progress, total);     // タイムアウト値設定 Time out setting
+            }
+            else
+            {
+                toolStripProgressBar1.Value = (progress + 1) * 100 / total;
+            }
+        }
+        private void update_err(String line)
+        {
+            if (InvokeRequired)
+            {
+                this.BeginInvoke(new Action<string>(this.update_err), line);     // タイムアウト値設定 Time out setting
+            }
+            else
+            {
+                if (line != "")
+                    toolStripStatusLabel_Exception.Text = line;
+            }
+        }
+        private void update_player_Log(String line)
+        {
+            if (InvokeRequired)
+            {
+                this.BeginInvoke(new Action<string>(this.update_player_Log), line);     // タイムアウト値設定 Time out setting
+            }
+            else
+            {
+                if (line != "")
+                    textBox_player.AppendText(line + Environment.NewLine);
+            }
+        }
+        private void update_uuid_Log(String line)
+        {
+            if (InvokeRequired)
+            {
+                this.BeginInvoke(new Action<string>(this.update_uuid_Log), line);     // タイムアウト値設定 Time out setting
+            }
+            else
+            {
+                if (line != "")
+                    textBox_uuid.AppendText(line + Environment.NewLine);
+            }
+        }
+        private void update_btn(bool status)
+        {
+            if (InvokeRequired)
+            {
+                this.BeginInvoke(new Action<bool>(this.update_btn), status);     // タイムアウト値設定 Time out setting
+            }
+            else
+            {
+                button1.Enabled = status;
+            }
+        }
+    }
+}
